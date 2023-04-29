26b886c07242b8ad244667d45edb345aba1413f8
Matthew Lock <matthew_lock20@hotmail.com>
2023-04-29 12:57:46 +0100

    Initial Commit

---
+++ Under The Weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Under The Weather
+//
+//  Created by Matthew Lock on 29/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Under The WeatherUITests/Under_The_WeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Under_The_WeatherUITestsLaunchTests.swift
+//  Under The WeatherUITests
+//
+//  Created by Matthew Lock on 29/04/2023.
+//
+
+import XCTest
+
+final class Under_The_WeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
