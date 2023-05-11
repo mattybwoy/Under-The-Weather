4b4c74a6614f0205ece07eec2cc87f4eaef4e131
Matthew Lock <matthew_lock20@hotmail.com>
2023-05-11 19:41:35 +0100

    Refactored View, added textfield

--- Under The Weather/ViewControllers/LaunchScreenViewController.swift
+++ Under The Weather/ViewControllers/LaunchScreenViewController.swift
@@ -11,44 +11,8 @@ class LaunchScreenViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupBackgroundGradient()
-        setupViews()
-        
+        self.view = LaunchScreenView()
     }
     
-    func setupViews() {
-        view.backgroundColor = .blue
-        let launch = LaunchScreenView()
-        view.addSubview(launch.title)
-        launch.title.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            launch.title.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            launch.title.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -150),
-            launch.title.heightAnchor.constraint(equalToConstant: 50),
-            launch.title.widthAnchor.constraint(equalToConstant: 300)
-        ])
-        
-        view.addSubview(launch.titleImage)
-        launch.titleImage.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            launch.titleImage.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
-            launch.titleImage.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -270),
-            launch.titleImage.heightAnchor.constraint(equalToConstant: 250),
-            launch.titleImage.widthAnchor.constraint(equalToConstant: 250)
-        ])
-    }
-    
-    func setupBackgroundGradient() {
-        let colorTop =  UIColor(red: 66.0/255.0, green: 179.0/255.0, blue: 210.0/255.0, alpha: 1.0).cgColor
-        let colorBottom = UIColor(red: 3.0/255.0, green: 73.0/255.0, blue: 164.0/255.0, alpha: 1.0).cgColor
-        
-        let gradientLayer = CAGradientLayer()
-        gradientLayer.colors = [colorTop, colorBottom]
-        gradientLayer.locations = [0.0, 1.0]
-        gradientLayer.frame = self.view.bounds
-        
-        self.view.layer.insertSublayer(gradientLayer, at:0)
-        
-    }
+
 }
--- Under The Weather/Views/LaunchScreenView.swift
+++ Under The Weather/Views/LaunchScreenView.swift
@@ -10,14 +10,55 @@ import UIKit
 class LaunchScreenView: UIView {
     
     public init() {
-        super.init(frame: CGRect())
+        super.init(frame: .zero)
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    let title: UILabel = {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupBackgroundGradient()
+        
+        addSubview(titleImage)
+        titleImage.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleImage.centerXAnchor.constraint(equalTo: centerXAnchor),
+            titleImage.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -270),
+            titleImage.heightAnchor.constraint(equalToConstant: 250),
+            titleImage.widthAnchor.constraint(equalToConstant: 250)
+        ])
+        
+        addSubview(title)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            title.centerXAnchor.constraint(equalTo: centerXAnchor),
+            title.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -150),
+            title.heightAnchor.constraint(equalToConstant: 50),
+            title.widthAnchor.constraint(equalToConstant: 300)
+        ])
+        
+        addSubview(openingText)
+        openingText.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            openingText.centerXAnchor.constraint(equalTo: centerXAnchor),
+            openingText.centerYAnchor.constraint(equalTo: centerYAnchor, constant: -60),
+            openingText.heightAnchor.constraint(equalToConstant: 150),
+            openingText.widthAnchor.constraint(equalToConstant: 250)
+        ])
+        
+        addSubview(cityTextField)
+        cityTextField.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            cityTextField.centerXAnchor.constraint(equalTo: centerXAnchor),
+            cityTextField.centerYAnchor.constraint(equalTo: centerYAnchor, constant: 20),
+            cityTextField.heightAnchor.constraint(equalToConstant: 50),
+            cityTextField.widthAnchor.constraint(equalToConstant: 250)
+        ])
+    }
+    
+    private let title: UILabel = {
         let title = UILabel()
         title.textColor = UIColor(named: "TitleTextColor")
         title.text = "Under The Weather"
@@ -26,9 +67,39 @@ class LaunchScreenView: UIView {
         return title
     }()
     
-    let titleImage: UIImageView = {
+    private let titleImage: UIImageView = {
         let titleImage = UIImageView()
         titleImage.image = UIImage(named: "UnderTheWeatherTransparent")
         return titleImage
     }()
+    
+    private let openingText: UILabel = {
+        let openingText = UILabel()
+        openingText.textColor = UIColor(named: "TitleTextColor")
+        openingText.text = "Please provide your local city"
+        openingText.font = .systemFont(ofSize: 20)
+        openingText.textAlignment = .center
+        openingText.numberOfLines = 0
+        return openingText
+    }()
+    
+    private let cityTextField: UITextField = {
+        let cityTextField = UITextField()
+        cityTextField.backgroundColor = .white
+        cityTextField.borderStyle = .roundedRect
+        return cityTextField
+    }()
+    
+    func setupBackgroundGradient() {
+        backgroundColor = .clear
+        let colorTop =  UIColor(red: 66.0/255.0, green: 179.0/255.0, blue: 210.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 3.0/255.0, green: 73.0/255.0, blue: 164.0/255.0, alpha: 1.0).cgColor
+
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = bounds
+        layer.addSublayer(gradientLayer)
+    }
+
 }
